aee448526c5730e90c2984abe9e01dcf1d73a53b
SimonaRa20 <simonaaragauskaite@gmail.com>
2022-04-14 22:37:15 +0300

    1 dalis

---
+++ 1DALIS/1DALIS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1DALIS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
