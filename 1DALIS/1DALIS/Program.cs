00618f32835068e928fa00d6a8a3ec81a948db60
SimonaRa20 <simonaaragauskaite@gmail.com>
2022-04-19 17:38:03 +0300

    recursive method

--- 1DALIS/1DALIS/Program.cs
+++ 1DALIS/1DALIS/Program.cs
@@ -8,5 +8,39 @@ namespace _1DALIS
         {
             Console.WriteLine("Hello World!");
         }
+
+        //public static int F(int n, int m)
+        //{
+        //    if(x == 0)
+        //    {
+        //        return y.Length;
+        //    }
+        //    if(y.Length == 0 && x.Length > 0)
+        //    {
+        //        return x.Length;
+        //    }
+        //    else
+        //    {
+        //        for(int n = 0; n < x.Length; n++)
+        //        {
+        //            for(int m = 0; m < y.Length; m++)
+        //            {
+        //                int value1 = 1 + F(x[m-1],n)
+        //            }
+        //        }
+        //    }
+        //}
+
+        public static int D(int x, int y)
+        {
+            if (x == y)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
     }
 }
--- 3DALIS/3DALIS/Program.cs
+++ 3DALIS/3DALIS/Program.cs
@@ -6,64 +6,33 @@ namespace _3DALIS
     {
         static void Main(string[] args)
         {
-            string A = "Ab3bd";
-            string B = "dA";
+            string symbolSequence = "Ab3bd";
 
-            Console.WriteLine("Minimum number of " +
-                              "operations required is " +
-                               minOps(A, B));
+            Console.WriteLine(findMinInsertions(symbolSequence.ToCharArray(), 0, symbolSequence.Length - 1));
+
         }
-        public static int minOps(string A, string B)
+        static int findMinInsertions(char[] str, int l, int h)
         {
-
-            // This parts checks whether
-            // conversion is possible or not
-            if (A.Length != B.Length)
+            // Base Cases
+            if (l > h)
             {
-                return -1;
+                return int.MaxValue;
+            }
+            if (l == h)
+            {
+                return 0;
+            }
+            if (l == h - 1)
+            {
+                return (str[l] == str[h]) ? 0 : 1;
             }
 
-            int i, j, res = 0;
-            int[] count = new int[256];
-
-            // count characters in A
-
-            // subtract count for every
-            // character in B
-            for (i = 0; i < A.Length; i++)
-            {
-                count[A[i]]++;
-                count[B[i]]--;
-            }
-
-            // Check if all counts become 0
-            for (i = 0; i < 256; i++)
-            {
-                if (count[i] != 0)
-                {
-                    return -1;
-                }
-            }
-
-            i = A.Length - 1;
-            j = B.Length - 1;
-
-            while (i >= 0)
-            {
-                // If there is a mismatch, then
-                // keep incrementing result 'res'
-                // until B[j] is not found in A[0..i]
-                if (A[i] != B[j])
-                {
-                    res++;
-                }
-                else
-                {
-                    j--;
-                }
-                i--;
-            }
-            return res;
+            // Check if the first and last characters are same.
+            // On the basis of the comparison result, decide
+            // which subproblem(s) to call
+            return (str[l] == str[h]) ? findMinInsertions(str, l + 1, h - 1) :
+                                        (Math.Min(findMinInsertions(str, l, h - 1),
+                                        findMinInsertions(str, l + 1, h)) + 1);
         }
     }
 }
