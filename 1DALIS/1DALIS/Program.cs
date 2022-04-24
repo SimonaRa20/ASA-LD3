be76a88bba50aeb82e1c6f7a4dcc224c16de6c26
SimonaRa20 <simonaaragauskaite@gmail.com>
2022-04-24 18:01:24 +0300

    added dynamic programming method

--- 1DALIS/1DALIS/Program.cs
+++ 1DALIS/1DALIS/Program.cs
@@ -7,10 +7,12 @@ namespace _1DALIS
         static void Main(string[] args)
         {
             int m = F(1, 2);
-
+            int m2 = F2(1, 2);
             Console.WriteLine(m);
+            Console.WriteLine(m2);
         }
 
+        // using recursion
         public static int F(int n, int m)
         {
             if (n == 0)
@@ -26,7 +28,6 @@ namespace _1DALIS
                 return Math.Min(Math.Min(1 + F(m - 1, n), 1 + F(m, n - 1)), D(m, n) + F(m - 1, n - 1));
             }
         }
-
         public static int D(int i, int j)
         {
             int[] x = { 1, 5, 6, 7, 8, 9 };
@@ -39,6 +40,32 @@ namespace _1DALIS
             {
                 return 0;
             }
+        }
+
+        // using dynamic programinc 
+        static int F2(int M, int N)
+        {
+            var F = new int[M + 1, N + 1];
+
+            for (var m = 0; m <= M; m++)
+            {
+                for (var n = 0; n <= N; n++)
+                {
+                    if (n == 0)
+                    {
+                        F[m, n] = m;
+                        continue;
+                    }
+                    if (m == 0 && n > 0)
+                    {
+                        F[m, n] = n;
+                        continue;
+                    }
+                    F[m, n] = Math.Min((1 + F[m - 1, n]), Math.Min((1 + F[m, n - 1]), (D(m - 1, n - 1) + F[m - 1, n - 1])));
+                }
+            }
+
+            return F[M, N];
         }
     }
 }
